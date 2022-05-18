2e5dbdb4e6156729c8fdebeaa0038369f3050ffd
SnirGa <64706615+SnirGa@users.noreply.github.com>
2022-05-18 15:20:45 +0300

    updates

--- login.js
+++ login.js
@@ -2,9 +2,22 @@ function login(){
     let username=$("#loginusername").val();
     let pass=$("#loginpsw").val();
     if(map.get(username)==pass){
-        replace("settings")
+        alert("Succcesful Login");
+        replace("settings");
+        login_reset();
+        
     }
     else{
         alert("Please Try Again")
     }
+
+
+}
+
+function login_reset(){
+    $("#loginusername").val("");
+    $("#loginpsw").val("");
+
+
+
 }
